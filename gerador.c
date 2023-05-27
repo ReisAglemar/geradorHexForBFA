ce0fe37fcf4fdf91924d2f4e6730128865207e54
Reis <aglemar.reis@gmail.com>
2023-05-27 14:03:30 -0300

    first

---
+++ gerador.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    FILE *chavesGeradas; // ponteiro para criacao de arquivos
+    char caracter1 = 0;
+    char caracter2 = 0;
+    char caracter3 = 0;
+    char caracter4 = 0;
+    char caracter5 = 0;
+    char caracter6 = 0;
+    char caracter7 = 0;
+    char caracter8 = 0;
+    char caracter9 = 0;
+    char caracter10 = 0;
+    char caracter11 = 0;
+    char caracter12 = 0;
+    char caracter13 = 0;
+    char caracter14 = 0;
+    char caracter15 = 0;
+    char caracter16 = 0;
+    char caracter17 = 0;
+    char caracter18 = 0;
+    char caracter19 = 0;
+    char caracter20 = 0;
+    char caracter21 = 0;
+    char caracter22 = 0;
+    char caracter23 = 0;
+    char caracter24 = 0;
+    char caracter25 = 0;
+    char caracter26 = 0;
+    char caracter27 = 0;
+    char caracter28 = 0;
+    char caracter29 = 0;
+    char caracter30 = 0;
+    char caracter31 = 0;
+    char caracter32 = 0;
+
+    chavesGeradas = fopen("chavesGeradas.txt", "a"); // abre e cria o txt
+
+    for (caracter1 = 0X0; caracter1 <= 0XF; caracter1++)
+    {
+        for (caracter2 = 0X0; caracter2 <= 0XF; caracter2++)
+        {
+            for (caracter3 = 0X0; caracter3 <= 0XF; caracter3++)
+            {
+                for (caracter4 = 0X0; caracter4 <= 0XF; caracter4++)
+                {
+                    for (caracter5 = 0X0; caracter5 <= 0XF; caracter5++)
+                    {
+                        for (caracter6 = 0X0; caracter6 <= 0XF; caracter6++)
+                        {
+                            for (caracter7 = 0X0; caracter7 <= 0XF; caracter7++)
+                            {
+                                for (caracter8 = 0X0; caracter8 <= 0XF; caracter8++)
+                                {
+                                    for (caracter9 = 0X0; caracter9 <= 0XF; caracter9++)
+                                    {
+                                        for (caracter10 = 0X0; caracter10 <= 0XF; caracter10++)
+                                        {
+                                            for (caracter11 = 0X0; caracter11 <= 0XF; caracter11++)
+                                            {
+                                                for (caracter12 = 0X0; caracter12 <= 0XF; caracter12++)
+                                                {
+                                                    for (caracter13 = 0X0; caracter13 <= 0XF; caracter13++)
+                                                    {
+                                                        for (caracter14 = 0X0; caracter14 <= 0XF; caracter14++)
+                                                        {
+                                                            for (caracter15 = 0X0; caracter15 <= 0XF; caracter15++)
+                                                            {
+                                                                for (caracter16 = 0X0; caracter16 <= 0XF; caracter16++)
+                                                                {
+                                                                    for (caracter17 = 0X0; caracter17 <= 0XF; caracter17++)
+                                                                    {
+                                                                        for (caracter18 = 0X0; caracter18 <= 0XF; caracter18++)
+                                                                        {
+                                                                            for (caracter19 = 0X0; caracter19 <= 0XF; caracter19++)
+                                                                            {
+                                                                                for (caracter20 = 0X0; caracter20 <= 0XF; caracter20++)
+                                                                                {
+                                                                                    for (caracter21 = 0X0; caracter21 <= 0XF; caracter21++)
+                                                                                    {
+                                                                                        for (caracter22 = 0X0; caracter22 <= 0XF; caracter22++)
+                                                                                        {
+                                                                                            for (caracter23 = 0X0; caracter23 <= 0XF; caracter23++)
+                                                                                            {
+                                                                                                for (caracter24 = 0X0; caracter24 <= 0XF; caracter24++)
+                                                                                                {
+                                                                                                    for (caracter25 = 0X0; caracter25 <= 0XF; caracter25++)
+                                                                                                    {
+                                                                                                        for (caracter26 = 0X0; caracter26 <= 0XF; caracter26++)
+                                                                                                        {
+                                                                                                            for (caracter27 = 0X0; caracter27 <= 0XF; caracter27++)
+                                                                                                            {
+                                                                                                                for (caracter28 = 0X0; caracter28 <= 0XF; caracter28++)
+                                                                                                                {
+                                                                                                                    for (caracter29 = 0X0; caracter29 <= 0XF; caracter29++)
+                                                                                                                    {
+                                                                                                                        for (caracter30 = 0X0; caracter30 <= 0XF; caracter30++)
+                                                                                                                        {
+                                                                                                                            for (caracter31 = 0X0; caracter31 <= 0XF; caracter31++)
+                                                                                                                            {
+                                                                                                                                for (caracter32 = 0X0; caracter32 <= 0XF; caracter32++)
+                                                                                                                                {
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter1); // TODOS os fprint escreve a saida no txt
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter2);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter3);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter4);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter5);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter6);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter7);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter8);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter9);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter10);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter11);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter12);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter13);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter14);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter15);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter16);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter17);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter18);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter19);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter20);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter21);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter22);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter23);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter24);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter25);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter26);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter27);
+                                                                                                                                    fprintf(chavesGeradas, "%X-", caracter28);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter29);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter30);
+                                                                                                                                    fprintf(chavesGeradas, "%X", caracter31);
+                                                                                                                                    fprintf(chavesGeradas, "%X\n", caracter32);
+                                                                                                                                    printf("rodando...\n");
+                                                                                                                                }
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fclose(chavesGeradas); // fecha o txt
+    printf("Terminou!");
+    printf("\n\n");
+    return 0;
+}
